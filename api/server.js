772e82a1321d2e7c00abfebbbbec1d942244d932
thales.silva <thales.silva@anchieta.br>
2026-07-28 14:33:23 -0300

    a

---
+++ api/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+//atribui tudo do express na constante app
+const app = express();
+
+/* o get é um método http, pra fazer funcionar, 
+eu preciso tipo da rota (get) e o endereço, 
+exemplo: anchieta.br/cursos-50-mais */
+
+
+//req é a requisição (pergunta) e res, a resposta
+app.get('/usuarios', (req, res) => {
+   res.send('ok, deu bom')
+})
+
+app.listen(3000)
