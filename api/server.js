ef3387ab46be05017d982e41c2318bea55ede236
thales.silva <thales.silva@anchieta.br>
2026-07-28 14:35:07 -0300

    a

--- api/server.js
+++ api/server.js
@@ -3,10 +3,10 @@ import express from 'express'
 //atribui tudo do express na constante app
 const app = express();
 
+const users = []
 /* o get é um método http, pra fazer funcionar, 
 eu preciso tipo da rota (get) e o endereço, 
 exemplo: anchieta.br/cursos-50-mais */
-
 
 //req é a requisição (pergunta) e res, a resposta
 app.get('/usuarios', (req, res) => {
